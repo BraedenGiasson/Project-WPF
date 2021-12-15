666c34ad3efff67aa0ee0a707ebdd2daba4fbd70
Justus-Weidemann <78222536+Justus-Weidemann@users.noreply.github.com>
2021-12-15 18:21:21 -0500

    Justus - added comments to inventory class

--- WPF Project/WPF Project/Models/Inventory.cs
+++ WPF Project/WPF Project/Models/Inventory.cs
@@ -17,12 +17,21 @@ namespace WPF_Project.Models
         {
             inventoryList = new List<Model>(maxInventory);
         }
-
-        public static int GetAvailableParkingSpots()
+        /// <summary>
+        /// Method to get number of available parking spots
+        /// </summary>
+        /// <returns>
+        /// returns an integer of available spots
+        /// </returns>
+        public static int GetAvailableParkingSpots()        
         {
             return inventoryList.Count;
         }
 
+        /// <summary>
+        /// Method gets the index of the first available parking spot that can be filled with a car
+        /// </summary>
+        /// <returns> An integer of the index, -1 if not available parking spot is available </returns>
         public static int GetFirstAvailableParkingSpot()
         {
             for (int i = 0; i < inventoryList.Count; i++)
@@ -33,6 +42,13 @@ namespace WPF_Project.Models
             return -1;
         }
 
+
+        /// <summary>
+        /// Places a car in an available parking spot
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="freeSpot"></param>
+        /// <returns> Returns true if succesful or false if not </returns>
         public static bool SetParkingSpot(Model model, int freeSpot)
         {
             if (freeSpot < 0)
