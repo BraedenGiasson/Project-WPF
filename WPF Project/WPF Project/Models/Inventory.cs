fe153708f1f94ecd2d8e00053f1ae218ba3ba4ce
Justus-Weidemann <78222536+Justus-Weidemann@users.noreply.github.com>
2021-12-15 21:53:09 -0500

    Justus - Implemented inventory class again

--- WPF Project/WPF Project/Models/Inventory.cs
+++ WPF Project/WPF Project/Models/Inventory.cs
@@ -7,5 +7,40 @@ namespace WPF_Project.Models
 {
     internal class Inventory
     {
+
+        private static List<Model> inventoryList;
+        private static readonly int maxInventory = 100;
+
+        static Inventory()
+        {
+            inventoryList = new List<Model>(maxInventory);
+        }
+
+        public static int GetAvailableParkingSpots()
+        {
+            return inventoryList.Count;
+        }
+
+        public static int GetFirstAvailableParkingSpot()
+        {
+            for (int i = 0; i < inventoryList.Count; i++)
+            {
+                if (inventoryList[i] == null)
+                    return i;
+            }
+            return -1;
+        }
+
+        public static bool SetParkingSpot(Model model, int freeSpot)
+        {
+            if (freeSpot < 0)
+                return false;
+            else
+            {
+                inventoryList[freeSpot] = model;
+                return true;
+            }
+        }
+
     }
 }
