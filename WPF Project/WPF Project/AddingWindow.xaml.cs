a2c21fa367e9c0f90b4d4e16ff313e54066b7acc
Braeden Giasson <2043670@johnabbottcollege.net>
2021-12-15 20:08:36 -0500

    Added buttons to 'Adding' window, Added dropdown for model names, Added main window saving functionality when trying to close window.

--- WPF Project/WPF Project/AddingWindow.xaml.cs
+++ WPF Project/WPF Project/AddingWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WPF_Project.Models;
 
@@ -25,11 +26,26 @@ namespace WPF_Project
                             .Cast<Body>()
                             .ToList();
 
+            List<string> modelNames = new List<string>() 
+                { "A3", "A4", "A5", "A7", "A8", "Q3", "Q5", "Q7", "Q8", "R8", "TT", "e-tron", "e-tron GT", "Q4 e-tron" };
+
             InitializeComponent();
 
             // Binding
             cmbBodyType.ItemsSource = bodyTypes;
+            cmbModelNames.ItemsSource = modelNames;
             //txtColour.Visibility = Visibility.Hidden;
+        }
+
+        private void btnAddCar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            //NavigationService.Navigate(new MainWindow());
         }
     }
 }
--- WPF Project/WPF Project/MainWindow.xaml.cs
+++ WPF Project/WPF Project/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,8 +24,11 @@ namespace WPF_Project
     public partial class MainWindow : Window
     {
         private List<Model> models = new List<Model>();
-        
         List<Body> bodyTypes = new List<Body>();
+
+        // Saving 
+        private string saveLocation = string.Empty;
+        private bool saved = false;
 
         public MainWindow()
         {
@@ -59,6 +64,76 @@ namespace WPF_Project
         private void btnShoppingList_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void SaveLogic()
+        {
+            if (string.IsNullOrEmpty(saveLocation))
+            {
+                SaveFileDialog save = new SaveFileDialog();
+                save.Filter = "CVS Files|*.csv";//"Description|*.xyz|file2|*.file2";
+                if (save.ShowDialog() == true)
+                {
+                    saveLocation = save.FileName;
+                }
+                else
+                    return;
+            }
+            WriteDataToFile();
+        }
+
+        private void WriteDataToFile()
+        {
+            if (!saved)
+            {
+                try
+                {
+                    StringBuilder modelsCSV = new StringBuilder();
+                    foreach (Model visitor in models)
+                        //modelsCSV.AppendLine(visitor.CSVData);
+
+                    File.WriteAllText(saveLocation, modelsCSV.ToString());
+                    saved = true;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+        private bool ChecktoOpen()
+        {
+            if (saved)
+                return true;
+
+            if (string.IsNullOrEmpty(saveLocation) && models.Count == 0)
+                return true;
+
+
+            //Data is not saved
+            MessageBoxResult result =
+                MessageBox.Show("Do you want to save changes?", "Unsaved changes", MessageBoxButton.YesNoCancel, MessageBoxImage.Warning);
+
+
+            if (result == MessageBoxResult.No)
+                return true;
+
+            if (result == MessageBoxResult.Cancel)
+                return false;
+
+            if (result == MessageBoxResult.Yes)
+                SaveLogic();
+
+            return saved; //if the user saved it mean it ok to open a file and if the user cancels then saved will be false
+
+        }
+
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = !ChecktoOpen();
         }
     }
 }
