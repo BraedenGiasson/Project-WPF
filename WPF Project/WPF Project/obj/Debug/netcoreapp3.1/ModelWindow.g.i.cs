6b6705a5a69d02c404ff6f20d4cd69dbf7c49c22
Braeden Giasson <2043670@johnabbottcollege.net>
2021-12-16 21:50:24 -0500

    need to commit

--- WPF Project/WPF Project/obj/Debug/netcoreapp3.1/ModelWindow.g.i.cs
+++ WPF Project/WPF Project/obj/Debug/netcoreapp3.1/ModelWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace WPF_Project {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPF Project;component/modelwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPF Project;V1.0.0.0;component/modelwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\ModelWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
